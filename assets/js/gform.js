37601c9684c1d48cc12a4923a6d735fbcdaf59cb
Aditya Srivastava <69895067+JarvesAS10@users.noreply.github.com>
2023-10-17 01:15:44 +0530

    update 6 10/17/2023
    gform js file

--- assets/js/gform.js
+++ assets/js/gform.js
@@ -1,10 +1,11 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzF02OSKaLbTop-gJ75WdPSN44CdX9MzRsNJgehaXDFAIGGnsoggp2PQZ5Fx8nU8e7sUg/exec'
+
             const form = document.forms['google-sheet']
-          
+            
             form.addEventListener('submit', e => {
               e.preventDefault()
               fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-                .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
-                .then(() => { window.location.reload(); })
-                .catch(error => console.error('Error!', error.message))
+              .then(response => alert("Thanks for Contacting us! We Will Contact You Soon..." ))
+              .then(() => { window.location.reload(); })
+              .catch(error => console.error('Error!', error.message))
             })
