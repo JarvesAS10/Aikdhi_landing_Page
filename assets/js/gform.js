0f69372b07b95a6399d8f057129bc6298ea24417
Aditya Srivastava <69895067+JarvesAS10@users.noreply.github.com>
2023-10-17 01:01:30 +0530

    Update 5 10/17/2023
    gform js file

--- assets/js/gform.js
+++ assets/js/gform.js
@@ -5,5 +5,6 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbzF02OSKaLbTop-gJ75Wd
               e.preventDefault()
               fetch(scriptURL, { method: 'POST', body: new FormData(form)})
                 .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+                .then(() => { window.location.reload(); })
                 .catch(error => console.error('Error!', error.message))
             })
